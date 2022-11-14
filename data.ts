c9211a98b3244b40ceb23bfa0988521e3444a26d
temanchib <85640181+temanchib@users.noreply.github.com>
2022-11-14 09:37:21 +0700

    Add files via upload
    fix-constan-bug

---
+++ data.ts
@@ -0,0 +1,116 @@
+import bookCase from '../Images/Bookcase.jpg';
+import officeChair from '../Images/Office Chair.png';
+import meetingTableBlue from '../Images/Table Blue.png';
+import meetingTableFull from '../Images/Meeting Table Blue (Full Aset).jpg';
+import singleLounge from '../Images/Single Lounge.jpeg';
+import tableYellow from '../Images/Table Yellow.png';
+import tableYellowSet from '../Images/Meeting Table Yellow (Set).jpeg';
+import officeLounge from '../Images/Office Lounge.jpeg';
+
+export const data =
+    [
+        {
+            "id": 1,
+            "title": "Bookcase",
+            "price": 75000,
+            "description": "Single Bookcase for Office",
+            "category": "",
+            "image": bookCase,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        },
+        {
+            "id": 2,
+            "title": "Office Chair",
+            "price": 50000,
+            "description": "Single Chair for Office",
+            "category": "",
+            "image": officeChair,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        },
+        {
+            "id": 3,
+            "title": "Meeting Table Blue",
+            "price": 50000,
+            "description": "Table for meeting with Blue Design",
+            "category": "",
+            "image": meetingTableBlue,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        },
+        {
+            "id": 4,
+            "title": "Meeting Table Blue (Full Aset)",
+            "price": 150000,
+            "description": "Table, Chair, Bookcase (One set)",
+            "category": "",
+            "image": meetingTableFull,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        },
+        {
+            "id": 5,
+            "title": "Single Lounge",
+            "price": 50000,
+            "description": "Single Lounge for Office",
+            "category": "",
+            "image": singleLounge,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        },
+        {
+            "id": 6,
+            "title": "Meeting Table Yellow",
+            "price": 50000,
+            "description": "Table for meeting with Yellow Design",
+            "category": "",
+            "image": tableYellow,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        },
+        {
+            "id": 7,
+            "title": "Meeting Table Yellow (Set)",
+            "price": 100000,
+            "description": "Table, Single Lounge (One set)",
+            "category": "",
+            "image": tableYellowSet,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        },
+        {
+            "id": 8,
+            "title": "Office Lounge",
+            "price": 100000,
+            "description": "Office Lounge for Office to relax",
+            "category": "",
+            "image": officeLounge,
+            "rating": {
+                "rate": 3.9,
+                "count": 120
+            },
+            "amount": 0
+        }
+    ];
